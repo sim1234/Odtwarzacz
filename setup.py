00bd22402bed707be0d6359aa9a8ccc0d16afcde
sim1234 <szymon1313@gmail.com>
2012-11-06 16:52:01 +0100

    RC3

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import py2exe
 from glob import glob
 
 setup(windows = ['prog.py'],
-      data_files=["config.txt", "przerwy.txt", "gdiplus.dll", "MSVCP90.dll"],
+      data_files=["config.txt", "przerwy.txt", "license.txt", "gdiplus.dll", "MSVCP90.dll"],
       name='Odtwarzacz',
       version='1.0',
       description='Music player, which turns on and off the music at specified time.',
       author='Szymon Zmilczak',
-      zipfile=None,
+      zipfile="data.lib",
       options = {"py2exe": {"compressed": 3, 
                             "optimize": 2,
-                            "bundle_files": 2
+                            "bundle_files": 3
                          }
               }
       #packages=['distutils', 'distutils.command', 'wx'],
